2b4016f7c32b992ba4676cd139e87fe930abf2ba
Ulf Urdén <ulf.urden@mittmedia.se>
2016-02-29 16:18:54 +0100

    Check in tokenmap, initial write support

--- blitz2.py
+++ blitz2.py
@@ -1,18 +1,33 @@
-from bb2map import keyWordToToken
 import sys
 
-def saveBlitz(data):
+kwToToken = dict()
+tokenToKw = dict()
+
+def load_mapfile(filename):
+	mapfile = open(filename, "r")
+	lines = mapfile.readlines()
+
+	for line in lines:
+		#print line
+		if not ("###" in line):
+			keyword, tokenStr = line.split(" = ")
+			token = tokenStr.strip().decode("hex")
+			kwToToken[keyword] = token
+			tokenToKw[token] = keyword
+
+def save(data):
 	out = str()
 	lines = data.split('\n')
 	for line in lines:
 		newLine = line
 		words = line.split(' ')
 		for word in words:
-			if word in keyWordToToken.keys():
-				newLine = newLine.replace(word, keyWordToToken[word])
+			if word in kwToToken.keys():
+				newLine = newLine.replace(word, kwToToken[word])
 		out += newLine + '\n'
 	print out
 
 if __name__ == '__main__':
-	if sys.argv[1] == '-s':
-		saveBlitz(open(sys.argv[2], "r").read())
+	# Usage: blitz.py mapfile infile outfile
+	load_mapfile(sys.argv[1])
+	save(open(sys.argv[2], "r").read())
